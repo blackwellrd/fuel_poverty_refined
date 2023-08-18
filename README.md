ea30aac156497b3f7626516fcbbd4d2540ab365f
Richard Blackwell <73821575+blackwellrd@users.noreply.github.com>
2023-08-18 16:51:06 +0100

    Partial commit

--- README.md
+++ README.md
@@ -1,48 +1,106 @@
-# Refined Fuel Poverty Risk
+# Fuel Poverty Programme
 
-**Author:** Richard Blackwell **Date:** 2023-06-27
+### Author: Richard Blackwell
 
-A refinement of the **[Department of Energy Security and Net Zero - Sub-regional fuel poverty in England 2023](https://www.gov.uk/government/statistics/sub-regional-fuel-poverty-data-2023-2021-data)** using **[NOMIS - Census 2021 data](https://www.nomisweb.co.uk/sources/census_2021)** and **[Office for National Statistics - Open Geography Portal](https://geoportal.statistics.gov.uk/)**
+### Email: richard.blackwell@swahsn.com
 
-### Acknowledgements
+### Date: 2023-08-18
 
-Source: Department of Energy Security and Net Zero
+----
 
-Source: NOMIS Official Census and Labour Market Statistics
+## Data
 
-Source: Office for National Statistics licensed under the Open Government Licence v.3.0
+### Fuel Poverty data
 
+Source: [Department for Energy Security and Net Zero](https://www.gov.uk/government/organisations/department-for-energy-security-and-net-zero)
+
+Landing Page: [Sub-Regional Fuel Poverty Data](https://www.gov.uk/government/statistics/sub-regional-fuel-poverty-data-2023-2021-data)
+
+Example Data: [2023 publication (2021 data)](https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1153252/sub-regional-fuel-poverty-tables-2023-2021-data.xlsx)
+
+Notes: Total number of households and number of those households in fuel poverty by 2021 Lower-layer Super Output Areas (LSOA 2021) using 2021 modelled data.
+
+### Household Deprivation data
+
+Source: [NOMIS](https://www.nomisweb.co.uk/sources/census_2021)
+
+Landing Page: [2021 Census Bulk Data Download](https://www.nomisweb.co.uk/sources/census_2021_bulk)
+
+Example Data: [TS011 - Households by deprivation dimensions](https://www.nomisweb.co.uk/output/census/2021/census2021-ts011.zip)
+
+Notes: The `census2021-ts011-oa.csv` file in the zip file is the file used in the calculation and contains the household deprivation data at 2021 Output Area (OA 2021) level.
+
+### 2021 Output Area to 2021 Lower-layer Super Output Area lookup data
+
+Source: [ONS Open Geography Portal](https://geoportal.statistics.gov.uk/)
+
+Landing Page: [Output Area (2021) Lookups](https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=-created&tags=all(LUP_OA_2021))
+
+Example Data: [December 2021 data](https://geoportal.statistics.gov.uk/datasets/ons::output-area-to-lower-layer-super-output-area-to-middle-layer-super-output-area-to-local-authority-district-december-2021-lookup-in-england-and-wales-v3/explore)
+
+Notes: The OA (2021) to LSOA (2021) data is used to link the fuel poverty data (published at LSOA 2021 level) to household deprivation data (published at OA 2021 level)
+
+### Sub ICB Locations to Integrated Care Boards to NHS England Region Lookup
+
+Source: [ONS Open Geography Portal](https://geoportal.statistics.gov.uk/)
+
+Landing Page: [Sub ICB Locations to to Integrated Care Boards to NHS England Regions](https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=-created&tags=all(LUP_SICBL_ICB_NHSER))
+
+Example Data: [April 2023 data](https://geoportal.statistics.gov.uk/datasets/ons::sub-icb-locations-to-integrated-care-boards-to-nhs-england-region-april-2023-lookup-in-england/explore)
+
+Notes: This data is used to create the postcode to refined fuel poverty risk for individual ICB by NHS England Region output files
+
+### ONS Postcode data
+
+Source: [ONS Open Geography Portal](https://geoportal.statistics.gov.uk/)
+
+Landing Page: [ONS Postcode Directory](https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=-created&tags=all(PRD_ONSPD%2CMAY_2023))
+
+Example Data: [May 2023 data](https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-may-2023/about)
+
+Notes: The `ONSPD_MAY_2023_UK.csv` file in the `Data` directory in the zip file is is used to map active postocde to Output Area 2021 in order to map patient's postcodeto fuel poverty risk.
+
+----
 
 ## Methodology
 
-+ Datasets
-  + Source: Department of Energy Security and Net Zero
-    + Data: Sub-regional Fuel Poverty 2023
-      + Measure: Proportion of households at risk of fuel poverty (from total number of households and number of households at risk)
-      + Period: 2021
-      + Geography: Lower-layer Super Output Area (LSOA) 2021
-  + Source: NOMIS
-    + Data: Household Deprivation
-      + Measure: Number households deprived in zero, 1, 2, 3 or all 4 dimensions
-      + Period: 2021 Census
-      + Geography: Output Area (OA) 2021
-  + Source: Office for National Statistics
-    + Data: Census Output Area (OA) 2021 to LSOA 2021 lookup
+The fuel poverty risk is defined as the percentage of households in each Lower-layer Super Output Area (2021) deemed to be fuel poor and this is linked to the 2021 Census household deprivation data using the output area to lower-layer super output area lookup. This linked data is then ordered in the following way
 
-### Gathering the data
+ - Percentage of households deemed to by fuel poor in **descending** order (LSOA level)
+ - Percentage of households deemed to be deprived in all four dimensions of household deprivation used in the 2021 census; education, employment, health and housing in **descending** order (OA level)
+ - Percentage of households deemed to be deprived in three of the four dimensions of household deprivation in **descending** order (OA level)
+ - Percentage of households deemed to be deprived in two of the four dimensions of household deprivation in **descending** order (OA level) 
+ - Percentage of households deemed to be deprived in one of the four dimensions of household deprivation in **descending** order (OA level) 
+  - Percentage of households deemed to be deprived in none of the four dimensions of household deprivation in **ascending** order (OA level) 
 
-The first step is to obtain the data from the relevant organisations, provided in this repository are the relevant downloads required to refine the fuel poverty measure but if updates are required or you wish add other data to enhance the process the links above will take you to the main pages for the data from those organisations.
+A ascending rank is then applied to this order list so the first output area listed (rank = 1) will be the most at risk in terms of the refined fuel poverty indicator (i.e fuel poverty and deprivation) and the last output area listed (rank = 178,605) will be the least at risk.
 
-### Standardising geography areas
+For each NHS England region the and for each Integrated Care Board in that region the postcode that are in that area are matched to the refined fuel poverty risk by using the output area and these list are output as comma separated value (csv) files
 
-The next step is to ensure that we has consistancy across geographical areas. The fuel poverty data is at LSOA level and the census data is at OA level. If we use the same fuel poverty data for the constituent output areas of the lower-layer super output area we will then have all our data at output area.
+----
 
-In order to do this we join the output area to lower-layer super output area lookup data to the fuel poverty data to add in the output area detail.
+## Outputs
 
-### Processing the data
+### Full refined fuel poverty risk data
 
-The penultimate step is to convert the data into a ranking, this is done simply by ordering the data in descending order (the area ranked 1 has the highest need) by fuel poverty risk and then the proportion of households deprived in all 4 dimensions, 3, 2 and only 1.
+The file fp_refined.csv in the fp_refined.zip file consisted of the following fields
 
-### Combining the data
+ - `oa21cd`, `lsoa21cd` and `msoa21cd`	- 2021 Output Area, Lower-Layer Super Output Area and Middle-layer Super Output Area codes respectively
+ - `lad22nm` - 2022 Local Authority Name 
+ - `households_lsoa` - Number of households in LSOA from fuel poverty data	
+ - `households_in_fp` - Number of households in LSOA deemed to be fuel poor
+ - `fp_pct` - Percentage of households in LSOA deemed to be fuel poor
+ - `households_oa` - Number of households in OA from census household deprivation data
+ - `hd_pct_0`, `hd_pct_1`, `hd_pct_2`, `hd_pct_3` and `hd_pct_0` - Percentage of households deprived in OA deprived in 0, 1, 2, 3 and all 4 dimensions respectively
+ - `fp_rank` - Rank of percentage of households in LSOA deemed to be fuel poor (where rank 1 is the LSOA with the highest percentage of households deemed to be fuel poor)
+ - `hp4_rank`, `hp3_rank`, `hp2_rank` and `hp1_rank` - Rank of percentage of households in OA deprived in all 4, 3, 2 and 1 dimension respectively (where rank 1 is the OA with the highest percentage of households deprived in that number of dimensions)
+ - `hp0_rank`	- Rank of percentage of households in OA not deprived in any dimension (where rank 1 is the OA with the **lowest** percentage of households not deprived in any dimension)
+ - `overall_rank`	
+ - `fp_decile`	
+ - `hp4_decile`	
+ - `hp3_decile`	
+ - `hp2_decile`	
+ - `hp1_decile`	
+ - `hp0_decile`	
+ - `overall_decile`
 
-The final stage is to add an overall rankings. In addition to the rank we will also calculate the deciles for each metric and overall for use in choropleth mapping.
--- fuel_poverty_refined.R
+++ fuel_poverty_refined.R
@@ -1,91 +1,162 @@
-#*******************************************************************#
-#                                                                   #
-# File: fuel_poverty_refined.R                                      #
-# Desc: Refine the BEIS sub-regional fuel poverty England 2022 with #
-#       the NOMIS 2021 Census data on household deprivation.        #
-# Auth: Richard Blackwell                                           #
-# Date: 2027-06-18                                                  #
-#                                                                   #
-#*******************************************************************#
-
-# 1. Load libraries ----
-# **********************
-library(tidyverse)
-library(readxl)
-
-# 2. Load data ----
-# *****************
-
-fuel_poverty_excel_workbook <- './data/fp/sub-regional-fuel-poverty-tables-2023-2021-data.xlsx'
-fuel_poverty_excel_worksheet <- 'Table 3'
-household_deprivation_csvfile <- './data/census/census2021-ts011-oa.csv'
-
-# * 2.1. Fuel poverty ----
-# ````````````````````````
-# Read in the LSOA level fuel poverty data from the Table 3 worksheet 
-# ignoring the first 2 lines. Select only the essential fields and rename them, 
-# and finally filter to English LSOAs only
-df_fp <- read_excel(path = fuel_poverty_excel_workbook, 
-                    sheet = fuel_poverty_excel_worksheet,
-                    skip = 2) %>%
-  select(1, 6:7) %>%
-  rename_with(.fn = ~c('lsoa21cd', 'households', 'households_in_fp')) %>%
-  filter(grepl('^E',lsoa21cd))
-
-# * 2.2. Household deprivation ----
-# `````````````````````````````````
-# Read in the OA level household deprivation data, select only the essential
-# fields and rename them. Finally calculate the percentage of households deprived 
-# in zero through to all four domains.
-df_hd <- read.csv(household_deprivation_csvfile, header = TRUE) %>% 
-  select(-c(1, 3)) %>%
-  rename_with(.fn = ~c('oa21cd', 'households', 'dim_0', 'dim_1', 'dim_2', 'dim_3', 'dim_4')) %>%
-  mutate(across(.cols = 3:7, .fns = ~.x/households, .names = 'pct_{.col}')) %>%
-  select(-c(3:7))
-
-# * 2.3. OA21 to LSOA21 lookup ----
-# `````````````````````````````````
-# Read in the OA21 to LSOA21 lookup, keeping only the essential fields and rename them.
-df_oa21_lsoa21_msoa21 <- read.csv('.\\data\\lu\\Output_Area_to_Lower_layer_Super_Output_Area_to_Middle_layer_Super_Output_Area_to_Local_Authority_District_(December_2021)_Lookup_in_England_and_Wales_v3.csv') %>% 
-  select(1, 2, 5, 9) %>%
-  rename_with(.fn = ~c('oa21cd', 'lsoa21cd', 'msoa21cd', 'lad22nm'))
-
-# 3. Process the fuel poverty risk data ----
-# ******************************************
-
-# Join the LSOA level fuel poverty data to the OA21 to LSOA21 lookup to obtain OA level fuel poverty data
-df_fp <- df_fp %>% 
-  inner_join(df_oa21_lsoa21_msoa21, by = 'lsoa21cd') %>% 
-  # and then join to the household deprivation data
-  inner_join(df_hd, by = 'oa21cd') %>%
-  transmute(oa21cd, lsoa21cd, msoa21cd, lad22nm, 
-            households_lsoa = households.x, households_in_fp, fp_pct = households_in_fp/households.x,
-            households_oa = households.y, hd_pct_0 = pct_dim_0,
-            hd_pct_1 = pct_dim_1, hd_pct_2 = pct_dim_2,
-            hd_pct_3 = pct_dim_3, hd_pct_4 = pct_dim_4) %>%
-  # Order by fuel poverty, then most deprived in 4 dimension, 3, 2, 1 all descending and finally zero ascending
-  arrange(desc(fp_pct), 
-          desc(hd_pct_4), desc(hd_pct_3), 
-          desc(hd_pct_2), desc(hd_pct_1), hd_pct_0) %>%
-  # Add in a rank and a decile for all the variables
-  mutate(
-    fp_rank = rank(desc(fp_pct), ties.method = 'average'),
-    hp4_rank = rank(desc(hd_pct_4), ties.method = 'average'),
-    hp3_rank = rank(desc(hd_pct_3), ties.method = 'average'),
-    hp2_rank = rank(desc(hd_pct_2), ties.method = 'average'),
-    hp1_rank = rank(desc(hd_pct_1), ties.method = 'average'),
-    hp0_rank = rank(desc(hd_pct_4), ties.method = 'average'),
-    overall_rank = seq_along(oa21cd),
-    fp_decile = ntile(desc(fp_pct), n = 10),
-    hp4_decile = ntile(desc(hd_pct_4), n = 10),
-    hp3_decile = ntile(desc(hd_pct_3), n = 10),
-    hp2_decile = ntile(desc(hd_pct_2), n = 10),
-    hp1_decile = ntile(desc(hd_pct_1), n = 10),
-    hp0_decile = ntile(desc(hd_pct_4), n = 10)
-  ) %>% 
-  mutate(overall_decile = ntile(overall_rank, n = 10)) 
-
-# 4. Output results ----
-# **********************
-write.csv(df_fp, 'fp_refined.csv')
-zip('fp_refined.zip', 'fp_refined.csv', flags = "-m")
+  #*******************************************************************#
+  #                                                                   #
+  # File: fuel_poverty_refined.R                                      #
+  # Desc: Refine the BEIS sub-regional fuel poverty England 2022 with #
+  #       the NOMIS 2021 Census data on household deprivation.        #
+  # Auth: Richard Blackwell                                           #
+  # Date: 2027-06-18                                                  #
+  #                                                                   #
+  #*******************************************************************#
+  
+  # 1. Load libraries ----
+  # **********************
+  library(tidyverse)
+  library(readxl)
+  
+  # 2. Load data ----
+  # *****************
+  
+  # These file locations should be altered to reflect the local installation
+  fuel_poverty_workbook <- './data/fp/sub-regional-fuel-poverty-tables-2023-2021-data.xlsx'
+  fuel_poverty_worksheet <- 'Table 3'
+  household_deprivation_csvfile <- './data/census/census2021-ts011-oa.csv'
+  output_area_lookup_file <- './data/lu/Output_Area_to_Lower_layer_Super_Output_Area_to_Middle_layer_Super_Output_Area_to_Local_Authority_District_(December_2021)_Lookup_in_England_and_Wales_v3.csv'
+  icb_lookup_file <- './data/lu/Sub_ICB_Locations_to_Integrated_Care_Boards_to_NHS_England_(Region)_(April_2023)_Lookup_in_England.csv'
+  postcode_file <- './data/pcd/ONSPD_MAY_2023_UK.csv'
+  outputdir <- './output'
+  
+  # * 2.1. Fuel poverty ----
+  # ````````````````````````
+  # Read in the LSOA level fuel poverty data from the Table 3 worksheet 
+  # ignoring the first 2 lines. Select only the essential fields and rename them, 
+  # and finally filter to English LSOAs only
+  df_fp <- read_excel(path = fuel_poverty_workbook, 
+                      sheet = fuel_poverty_worksheet,
+                      skip = 2) %>%
+    select(1, 6:7) %>%
+    rename_with(.fn = ~c('lsoa21cd', 'households', 'households_in_fp')) %>%
+    filter(grepl('^E',lsoa21cd))
+  
+  # * 2.2. Household deprivation ----
+  # `````````````````````````````````
+  # Read in the OA level household deprivation data, select only the essential
+  # fields and rename them. Finally calculate the percentage of households deprived 
+  # in zero through to all four domains.
+  df_hd <- read.csv(household_deprivation_csvfile, header = TRUE) %>% 
+    select(-c(1, 3)) %>%
+    rename_with(.fn = ~c('oa21cd', 'households', 'dim_0', 'dim_1', 'dim_2', 'dim_3', 'dim_4')) %>%
+    mutate(across(.cols = 3:7, .fns = ~.x/households, .names = 'pct_{.col}')) %>%
+    select(-c(3:7))
+  
+  # * 2.3. OA21 to LSOA21 lookup ----
+  # `````````````````````````````````
+  # Read in the OA21 to LSOA21 lookup, keeping only the essential fields and rename them.
+  df_oa21_lsoa21_msoa21 <- read.csv(output_area_lookup_file) %>% 
+    select(1, 2, 5, 9) %>%
+    rename_with(.fn = ~c('oa21cd', 'lsoa21cd', 'msoa21cd', 'lad22nm'))
+  
+  # * 2.4. ICB lookup ----
+  # ``````````````````````
+  # Read in the ICB lookup, keeping only the essential distinct fields and rename them.
+  df_icb <- read.csv(path = icb_lookup_file) %>% 
+    select(4:9) %>%
+    distinct() %>% 
+    rename_with(.fn = ~c('ons_icbcd', 'icbcd', 'icbnm', 'ons_nhsercd', 'nhsercd', 'nhsernm')) %>%
+    mutate(icb_label = gsub('_Integrated_Care_Board', 
+                            '_ICB', 
+                            gsub('__', 
+                                 '_', 
+                                 gsub('\\W', 
+                                      '_', 
+                                      paste0(df_icb$icbcd, '_', df_icb$icbnm)))),
+           nhser_label = gsub('__', 
+                              '_', 
+                              gsub('\\W', 
+                                   '_', 
+                                   paste0(df_icb$nhsercd, '_', df_icb$nhsernm))))
+  
+  
+  # * 2.5. Postcode data ----
+  # `````````````````````````
+  # Read in the postcode data, keeping only the essential fields and rename them and filter on
+  # postcodes that are active and linked to an English ICB.
+  df_pcd <- read.csv(postcode_file) %>% 
+    select(3, 51, 50, 5) %>%
+    rename_with(.fn = ~c('pcds', 'oa21cd', 'icbcd', 'doterm')) %>%
+    filter(icbcd!='' & grepl('^E', icbcd) & is.na(doterm)) %>%
+    select(-doterm)
+  
+  # 3. Process the fuel poverty risk data ----
+  # ******************************************
+  
+  # Join the LSOA level fuel poverty data to the OA21 to LSOA21 lookup to obtain OA level fuel poverty data
+  df_fp <- df_fp %>% 
+    inner_join(df_oa21_lsoa21_msoa21, by = 'lsoa21cd') %>% 
+    # and then join to the household deprivation data
+    inner_join(df_hd, by = 'oa21cd') %>%
+    transmute(oa21cd, lsoa21cd, msoa21cd, lad22nm, 
+              households_lsoa = households.x, households_in_fp, fp_pct = households_in_fp/households.x,
+              households_oa = households.y, hd_pct_0 = pct_dim_0,
+              hd_pct_1 = pct_dim_1, hd_pct_2 = pct_dim_2,
+              hd_pct_3 = pct_dim_3, hd_pct_4 = pct_dim_4) %>%
+    # Order by fuel poverty, then most deprived in 4 dimension, 3, 2, 1 all descending and finally zero ascending
+    arrange(desc(fp_pct), 
+            desc(hd_pct_4), desc(hd_pct_3), 
+            desc(hd_pct_2), desc(hd_pct_1), hd_pct_0) %>%
+    # Add in a rank and a decile for all the variables
+    mutate(
+      fp_rank = rank(desc(fp_pct), ties.method = 'average'),
+      hp4_rank = rank(desc(hd_pct_4), ties.method = 'average'),
+      hp3_rank = rank(desc(hd_pct_3), ties.method = 'average'),
+      hp2_rank = rank(desc(hd_pct_2), ties.method = 'average'),
+      hp1_rank = rank(desc(hd_pct_1), ties.method = 'average'),
+      hp0_rank = rank(asc(hd_pct_0), ties.method = 'average'),
+      overall_rank = seq_along(oa21cd),
+      fp_decile = ntile(desc(fp_pct), n = 10),
+      hp4_decile = ntile(desc(hd_pct_4), n = 10),
+      hp3_decile = ntile(desc(hd_pct_3), n = 10),
+      hp2_decile = ntile(desc(hd_pct_2), n = 10),
+      hp1_decile = ntile(desc(hd_pct_1), n = 10),
+      hp0_decile = ntile(asc(hd_pct_0), n = 10)
+    ) %>% 
+    mutate(overall_decile = ntile(overall_rank, n = 10))
+  
+  # 4. Output results ----
+  # **********************
+  
+  # Create the output directory
+  dir.create(outputdir, showWarnings = FALSE, recursive = TRUE)
+  
+  # Write the fuel poverty risk data frame and zip
+  write.csv(df_fp %>% arrange(overall_rank), paste0(outputdir, '/fp_refined.csv'), row.names = FALSE)
+  zip(paste0(outputdir, '/fp_refined.zip'), paste0(outputdir, '/fp_refined.csv'), flags = "-m")
+  
+  # Iterate through each NHS England Region and output a file for each ICB and zip 
+  for(nhser in df_icb %>% distinct(ons_nhsercd) %>% .$ons_nhsercd){
+    d <- paste0(outputdir, 
+                '/', 
+                df_icb %>% 
+                  filter(ons_nhsercd==nhser) %>% 
+                  distinct(nhser_label) %>% 
+                  .$nhser_label) 
+    dir.create(d, showWarnings = FALSE, recursive = TRUE)
+    for(icb in df_icb %>% filter(ons_nhsercd==nhser) %>% .$ons_icbcd){
+      f <- paste0(d, 
+                  '/', 
+                  df_icb %>% 
+                    filter(ons_icbcd==icb) %>% 
+                    .$icb_label, 
+                  '.csv')
+      write.csv(df_pcd %>% 
+                  filter(icbcd == icb) %>%
+                  inner_join(df_fp %>% 
+                               select(oa21cd, fp_pct, overall_rank, overall_decile),
+                             by = 'oa21cd') %>%
+                  arrange(pcds),
+                f,
+                row.names = FALSE)
+      zip(gsub('\\.csv', '\\.zip', f), f, flags = "-mjD")
+    }
+  }
+  
+  
